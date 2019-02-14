101cbf4ad69a4f2a2e85d6dff10926b90cd1e24a
ldwformat <ldwformat@gmail.com>
2019-02-14 14:30:50 +0800

    chore: init

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+const Configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ["@commitlint/config-conventional"],
+  /*
+   * Resolve and load @commitlint/format from node_modules.
+   * Referenced package must be installed
+   */
+  formatter: "@commitlint/format"
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+};
+
+module.exports = Configuration;
