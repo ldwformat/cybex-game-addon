bbfdb1fc9a8d7a589cb5c9e2a6700f20cd634ee7
ldwformat <ldwformat@gmail.com>
2019-02-14 16:34:13 +0800

    chore: add jest config

---
+++ src/index.spec.ts
@@ -0,0 +1,11 @@
+describe("Some test", () => {
+  it("Do some right test", done => {
+    expect(0).toBe(0);
+    done();
+  });
+
+  it("Do some wrong test", done => {
+    expect(0).toBe(0);
+    done();
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 let a = function() {};
 a();
+"asdf";
