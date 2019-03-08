d8ca12100903fd6bd1d578cb3b35d789eea76012
ldwformat <ldwformat@gmail.com>
2019-03-08 12:11:44 +0800

    feat(qrcode): 以img元素展示QRcode

--- src/components/qrcode.tsx
+++ src/components/qrcode.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import QRCode from "qrcode.react";
-import { Button, Grid } from "@material-ui/core";
+import { Button, Grid, Typography } from "@material-ui/core";
 import { CloudDownload } from "@material-ui/icons";
 import { Dict } from "../providers/i18n";
 import { withTranslation, WithTranslation } from "react-i18next";
@@ -13,6 +13,7 @@ export const QRCodeDisplay = withTranslation()(
     } & WithTranslation
   > {
     qrcode: HTMLDivElement | undefined | null;
+    qrcodeImg: HTMLImageElement | undefined | null;
 
     download = (filename: string = "deposit.jpg") => {
       if (!this.qrcode) {
@@ -36,17 +37,64 @@ export const QRCodeDisplay = withTranslation()(
       });
     };
 
+    componentDidMount() {
+      this.updateQRImg();
+    }
+
+    componentDidUpdate() {
+      this.updateQRImg();
+    }
+
+    updateQRImg = () => {
+      if (this.qrcode && this.qrcodeImg) {
+        let canvas = this.qrcode.getElementsByTagName("canvas")[0];
+        if (!canvas) {
+          return;
+        }
+        canvas.toBlob(blob => {
+          if (!blob || !this.qrcodeImg) {
+            return;
+          }
+          let urlObj = URL.createObjectURL(blob);
+          this.qrcodeImg.src = urlObj;
+          let revoke = () => {
+            window.URL.revokeObjectURL(urlObj);
+            if (this.qrcodeImg) {
+              this.qrcodeImg.removeEventListener("load", revoke);
+            }
+          };
+          this.qrcodeImg.addEventListener("load", revoke);
+        });
+      }
+    };
+
+    isAppleDevice = () => {
+      return /iphone|ipad|ipod/i.test(navigator.userAgent);
+    };
+
     render() {
       let { text, filename, t } = this.props;
       return (
         <Grid container direction="column" alignContent="center">
           <div ref={qrcode => (this.qrcode = qrcode)}>
-            <QRCode value={text} />
+            <div style={{ display: "none" }}>
+              <QRCode value={text} />
+            </div>
+            <img
+              style={{ width: "12em", height: "12em" }}
+              ref={qrcodeImg => (this.qrcodeImg = qrcodeImg)}
+            />
           </div>
-          <Button color="secondary" onClick={_e => this.download(filename)}>
-            {t(Dict.SaveQRCode)}
-            <CloudDownload style={{ marginLeft: "4px" }} fontSize="small" />
-          </Button>
+          {!this.isAppleDevice() ? (
+            <Button color="secondary" onClick={_e => this.download(filename)}>
+              {t(Dict.SaveQRCode)}
+              <CloudDownload style={{ marginLeft: "4px" }} fontSize="small" />
+            </Button>
+          ) : (
+            <Typography align="center" style={{ margin: "0.5em" }}>
+              {t(Dict.SaveQRCodeLongPress)}
+            </Typography>
+          )}
         </Grid>
       );
     }
