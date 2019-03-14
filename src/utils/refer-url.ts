d1131528173da6b5646f634da5f7d47ca6f9e04a
ldwformat <ldwformat@gmail.com>
2019-03-14 18:50:23 +0800

    feat(refer): 临时增加Url的替换方式

--- src/utils/refer-url.ts
+++ src/utils/refer-url.ts
@@ -1,5 +1,10 @@
 import { resolvePath } from "./path";
+const RefReg = /\$\|refer\|/;
+
 export const getReferUrl = (path: string, accountName: string) =>
-  `${path}?refer=${accountName}`;
+  path.search(RefReg) !== -1
+    ? path.replace(RefReg, `refer=${accountName}`)
+    : `${path}?refer=${accountName}`;
+
 export const resolveNameFromReferUrl = (path: string) =>
-  (path.match(/\?refer\=(.+)(?:$|\&)/i) || [])[1];
+  (path.match(/\?refer\=(.+?)(?:$|\&|\#|\/)/i) || [])[1];
